b3c6da2e959a5a31af4e11559ca98c3457a13b91
Vladimir Timotijevic <vladimir@lioneve.com>
2023-02-01 19:55:51 +0100

    Single page created

---
+++ pages/SingleCaracter/[id].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import scss from "./SingleCaracter.module.scss";
+
+//types
+import { SingleCaracterProps } from '../../types/singleCaracter';
+
+const SingleCaracter: React.FC<SingleCaracterProps> = ({ caracter }) => {
+    return (
+        <div>SingleCaracter</div>
+    )
+}
+
+export default SingleCaracter;
---
+++ types/singleCaracter.ts
@@ -0,0 +1,3 @@
+export interface SingleCaracterProps {
+    caracter: any;
+}
